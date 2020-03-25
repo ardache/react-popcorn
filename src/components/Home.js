66bfa01ea9fff2aeca63a397b18a20cf9bf50949
Ardash <ardache@hotmail.com>
2020-03-24 20:25:05 -0600

    feauture: creating cursor and deploying on heroku

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import logo from '../logo.svg';
+import cursor from '../popcorn.svg';
 import { Link } from 'react-router-dom';
-import BranchCRUD from './services/question-service';
+import BranchCRUD from './services/branch-service';
+import { FaAccessibleIcon, FaBiohazard, FiHome, IoIosBicycle } from 'react-icons/fa';
 
 
 const Home = props => {
@@ -20,15 +22,18 @@ const Home = props => {
             <h3>Popcorn</h3>
             
             <header className="App-header">
-            <img src={logo} className="App-logo" width= "50px" alt="logo" />
+            <img src={cursor} className="App-logo" width= "100px" alt="logo" />
             <h1>Olvida todo lo que sabes <br></br>de <u>seguros</u> y protegete hoy.</h1>
             </header>
             <body>
-                <Link to="/hogar/1" className="StartButton">Ve nuestros precios</Link>
                 {
                     branch.map(topic => {
-                    return <Link to={`/hogar/1/${topic._id}`} className="StartButton">{topic.branch}}</Link>
-                    })
+                    return (
+                
+                        <Link to={`/hogar/1/${topic.next_question}`} className="StartButton" >
+                          <FaBiohazard/>  {topic.name}<br></br>
+                        </Link>
+                    )})
                 }
             </body>
           
